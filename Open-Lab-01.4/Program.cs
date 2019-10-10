4a0df6c644c67110825854f491e58736aead2168
z18ista08 <z18ista08@Z00P1-18.SPSKNM>
2019-10-10 09:01:49 +0200

    Add project files.

---
+++ Open-Lab-01.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Open_Lab_01._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3;
+            double b = 2;
+            double c = a * b / 2;
+            Console.WriteLine(c);
+        }
+    }
+}
